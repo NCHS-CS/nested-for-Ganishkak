f7f08f43e71133df835e7310160e3c20bdd23022
Ganishkak <2008562@apps.nsd.org>
2024-10-05 02:02:43 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,37 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
+    public static void main(String args[]){
+        //problem 1
+            for(int i = 1; i<=5; i++){
+                for(int x = 1; x<=i; x++){
+                System.out.print(i);
+            }
+            System.out.println();
+            }
+        //problem 2
+            for(int i = 1; i<=5; i++){
+                for(int x = 4; x>=i; x--){
+                    System.out.print(".");
+                }
+                for(int x = 1; x<=i;x++){
+                    System.out.print(i);
+                }
+                System.out.println();
+            }
+        //problem 3
+            for(int i = 1;i<=5;i++){
+                for(int x=5; x>i;x--){
+                    System.out.print("."); 
+                }
+                System.out.print(i);
+                
+                for(int x=1;x<i;x++){
+                    System.out.print(".");
+                }
+                System.out.println();
+            }
 
-package org.nhs.app;
+    }
+}
 
-// Your code goes here
+
+    
